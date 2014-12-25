6e57812152fdbf8c2ea3647ab351f789b3682167
bianhenghao <bianhenghao@cooee.cn>
2014-12-25 11:09:42 +0800

    UnitTest SwitchPagedView and gridLayout

---
+++ UnitTest/src/com/cooeeui/brand/zenlauncher/unittest/page/PageActivity.java
@@ -0,0 +1,21 @@
+
+package com.cooeeui.brand.zenlauncher.unittest.page;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.cooeeui.brand.zenlauncher.unittest.R;
+
+public class PageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.page);
+    }
+
+}
